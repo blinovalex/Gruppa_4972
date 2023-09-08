b63fe89c7c31c3b1f143e530cac3a0865df5b230
Aleksey Blinov <blinov71111@gmail.com>
2023-09-08 23:39:43 +0300

    Добавил Sem1Task7

---
+++ Sem1Task7/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите трехзначьное число: ");
+int num = int.Parse(Console.ReadLine()??"0");
+if(num > 100 && num < 1000)
+{
+    int lastPigit = num % 10;
+    Console.WriteLine("Последняя цифра числа " + num + " это число: " + lastPigit);
+}
+else
+{
+    Console.WriteLine("Число не трехзначное");
+}ё
