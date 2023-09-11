ad90cb1d53d998a8f96553e810bc85fc3f199603
Aleksey Blinov <blinov71111@gmail.com>
2023-09-11 19:27:53 +0300

    Добавил задание 10, 13, 15

---
+++ Sem2Task10/Program.cs
@@ -0,0 +1,14 @@
+﻿// №10 Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число: ");
+int num = int.Parse(Console.ReadLine() ?? "0");
+char[] data = num.ToString().ToArray();
+if (data.Length == 3)
+{
+    Console.WriteLine("Вторая цифра данного числа " + data[1]);
+}
+else
+{
+    Console.WriteLine("Вы вели не трехзначное число");
+}
---
+++ Sem2Task13/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа
+//  или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите  число: ");
+int num = int.Parse(Console.ReadLine()?? "0");
+char[] data = num.ToString().ToArray();
+if (data.Length >= 3)
+{
+    Console.WriteLine("Третия цифра данного числа " + data[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
+// Console.WriteLine(numA % 10);
