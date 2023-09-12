b6afd0d18a2098a5ff9bfa5d0dd5ffa9eb38ef61
Aleksey Blinov <blinov71111@gmail.com>
2023-09-12 21:34:31 +0300

    Добавил задания 10, 13,15

--- Sem2Task13/Program.cs
+++ Sem2Task13/Program.cs
@@ -3,7 +3,6 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
-
 Console.WriteLine("Введите  число: ");
 int num = int.Parse(Console.ReadLine()?? "0");
 char[] data = num.ToString().ToArray();
