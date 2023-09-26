d1145bb17425d0e8df7185efe9ee26fce060a64f
Aleksey Blinov <blinov71111@gmail.com>
2023-09-26 19:20:29 +0300

    Добавил домашнее задание семинара 5

---
+++ Sem5Task37/Program.cs
@@ -0,0 +1,43 @@
+﻿//Николай
+int[] GenArray(int num, int lowBorder, int highBorder)
+{
+    int[] array = new int[num];
+
+    for (int i = 0; i < num; i++)
+    {
+        array[i] = new Random().Next(lowBorder, highBorder + 1);
+    }
+
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    System.Console.Write("[ ");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        System.Console.Write(arr[i] + ", ");
+    }
+    System.Console.Write(arr[arr.Length - 1] + " ]");
+}
+
+int[] MultiPair(int[] array)
+{
+    int[] pairMass = new int[array.Length/2];
+
+    for (int i = 0; i < array.Length/2; i++)
+    {        
+        pairMass[i] = array[i] * array[array.Length -1 - i];
+    }
+
+    return pairMass;
+}
+
+// void PrintRezult(string msg)
+// {
+//     System.Console.WriteLine(msg);
+// }
+
+int[] arr = GenArray(12, -9, 9);
+PrintArray(arr);
+PrintArray(MultiPair(arr));
