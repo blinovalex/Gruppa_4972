d6e69b1e6291cbbaa8de84d520c6785481db1b69
Aleksey Blinov <blinov71111@gmail.com>
2023-10-07 11:34:29 +0300

    Исправил задачу 68

--- Sem9Task68/Program.cs
+++ Sem9Task68/Program.cs
@@ -12,7 +12,7 @@ int ReadData(string msg)
 int Akk(int m, int n)
 {
   if (m == 0) return n + 1;
-  else if (n == 0) return Ack(m - 1, 1);
+  else if (n == 0) return Akk(m - 1, 1);
   else return Akk(m - 1, Akk(m, n - 1));
 }
 
